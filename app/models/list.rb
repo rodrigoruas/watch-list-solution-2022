a47a275e54365e81775c45c8f95d23a5465efef4
Rodrigo Ruas <rodrigoruas2@gmail.com>
2022-01-28 11:35:48 +0000

    Rails app

---
+++ app/models/list.rb
@@ -0,0 +1,9 @@
+class List < ApplicationRecord
+  has_many :bookmarks, dependent: :destroy
+  has_many :movies, through: :bookmarks
+  has_many :reviews, dependent: :destroy
+
+  validates :name, uniqueness: true, presence: true
+
+  has_one_attached :photo
+end
